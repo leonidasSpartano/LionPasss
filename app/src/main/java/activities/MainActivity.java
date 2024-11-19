71580eec6b2a70d24dc2daaf46c38dc4b58f0d04
leonidasSpartano <leonardo199926@outlook.es>
2024-11-18 21:18:01 -0300

    se movio la activtie main a la carpeta activities

--- app/src/main/java/activities/MainActivity.java
+++ app/src/main/java/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.lionpass;
+package activities;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +10,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.lionpass.R;
 
 import UI.Login;
 
